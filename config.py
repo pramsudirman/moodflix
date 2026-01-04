916082017894bc39353d3c9c61a7909b2702053f
pramsudirman <pramirtha.sudirman@gmail.com>
2026-01-04 18:04:27 +0700

    Update config-sample.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Gemini API key
-GEMINI_API_KEY = "AIzaSyD3lwWXTXqLOggg6NGODzywNhaINfCzRj8"  # Replace with your actual Gemini API key
+GEMINI_API_KEY = ""  # Replace with your actual Gemini API key
 
 # OMDb API key
-OMDB_API_KEY = "82cb9e86"  # Replace with your actual OMDb API key
+OMDB_API_KEY = ""  # Replace with your actual OMDb API key
